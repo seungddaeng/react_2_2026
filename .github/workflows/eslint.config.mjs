87f0b56c046a1d0e9bc11d0c26cde857470f0735
seungddaeng <pattyquisbert26@gmail.com>
2026-08-19 18:40:21 -0400

    feat: add eslint config

---
+++ .github/workflows/eslint.config.mjs
@@ -0,0 +1,15 @@
+`eslint.config.mjs`
+
+```
+import js from ’@eslint/js’;
+import globals from ’globals’;
+export default [
+{
+files: [’**/*.js’],
+languageOptions: {
+globals: globals.browser
+}
+},
+js.configs.recommended
+];
+```
